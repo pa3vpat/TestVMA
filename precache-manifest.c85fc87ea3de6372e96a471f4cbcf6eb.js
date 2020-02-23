f6021fa4134c720e218913446b6abebec96deda2
pa3vpat <urkytsk.pro@gmail.com>
2020-02-23 16:12:30 +0300

    Updates

---
+++ precache-manifest.c85fc87ea3de6372e96a471f4cbcf6eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b298c36863a204c3e2f75a7d5a9d0fae",
+    "url": "/TestVMA/index.html"
+  },
+  {
+    "revision": "5d35aa40af89dadeffdc",
+    "url": "/TestVMA/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "2c3bfa21ef56e915e35d",
+    "url": "/TestVMA/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "5d35aa40af89dadeffdc",
+    "url": "/TestVMA/static/js/2.f0c92e7b.chunk.js"
+  },
+  {
+    "revision": "2c3bfa21ef56e915e35d",
+    "url": "/TestVMA/static/js/main.faf35c27.chunk.js"
+  },
+  {
+    "revision": "d822d007e62c62dfa530",
+    "url": "/TestVMA/static/js/runtime-main.bb3ef987.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/TestVMA/static/media/persik.4e1ec840.png"
+  }
+]);
